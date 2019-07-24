7edd01cc9a8586057bd1e20979b935181b5ff981
Mark Nunberg <mnunberg@haskalah.org>
2019-07-24 04:31:12 -0400

    rlookup: API robj objects are owned by us; no need to further dup them

--- src/spec.c
+++ src/spec.c
@@ -1148,6 +1148,7 @@ void *IndexSpec_RdbLoad(RedisModuleIO *rdb, int encver) {
       size_t dummy;
       char *s = RedisModule_LoadStringBuffer(rdb, &dummy);
       int rc = IndexAlias_Add(s, sp, 0, &status);
+      rm_free(s);
       assert(rc == REDISMODULE_OK);
     }
   }
